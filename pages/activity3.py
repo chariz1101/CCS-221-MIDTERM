a3d65a62d4249480dd31d34d5fc485c0a63a92b1
Markndrei <119771465+Markndrei@users.noreply.github.com>
2023-03-17 10:38:28 +0800

    Update activity3.py

--- pages/activity3.py
+++ pages/activity3.py
@@ -30,7 +30,7 @@ def rotation(images, degree):
    
     images = Image.open(images)
     images = np.asarray(images)
-    images rows = img_.shape[:2] 
+    cols, rows = img_.shape[:2] 
     rotated_image = cv2.warpPerspective(images, m_rotation_, (int(cols), int(rows)))
     plt.axis('off')
     plt.imshow(rotated_image)
@@ -82,6 +82,7 @@ def reflection(images, flip):
 
 
 def main():
+    st.title ('This is Activity 3)
     file_upload = st.sidebar.file_uploader('Upload your files here', ['png', 'jpg', 'webp'], True)
     option = st.sidebar.selectbox('What Image Manipulation Method to perform?', ('Translation', 'Rotation', 'Scaling', 'Shearing', 'Reflection'))
     st.write('The image manipulation you chose is:', option)
