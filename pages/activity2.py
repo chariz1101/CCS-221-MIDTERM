369a5edb46b7661dc5ee71f5f5b2dd2935a8543c
chiichann <116653047+chiichann@users.noreply.github.com>
2023-06-05 12:33:01 +0300

    Update activity2.py

--- pages/activity2.py
+++ pages/activity2.py
@@ -37,27 +37,8 @@ def flood_fill(x, y, replace, boundary_color):
                 ):
                     stack.append((neighbor_x, neighbor_y))
 
-    fig = plt.figure()
-    img = plt.imshow(two_d_arr, cmap='rainbow', interpolation='none')
-    img.set_clim([1, 1000])
-    plt.colorbar()
-    plt.show()
-    st.pyplot(fig)
-
-def fill(x, y, replace):
-    for i in range(len(two_d_arr)):
-        for j in range(len(two_d_arr[i])):
-            two_d_arr[x][y] = replace
-    
-    fig = plt.figure()
-    img = plt.imshow(two_d_arr, cmap='rainbow', interpolation='none')
-    img.set_clim([1, 1000])
-    plt.colorbar()
-    plt.show()
-    st.pyplot(fig)
-
 def main():
-    st.title("This is Activity 2")
+    st.title("This is Activity 2 and Flood Fill")
     
     activity_choice = st.sidebar.selectbox(
         "Select an activity",
@@ -74,20 +55,25 @@ def main():
         replace = st.sidebar.slider('color', 0, 1000, 500)
         st.write('color:', replace)
         
-        fill(x, y, replace)
+        for i in range(len(two_d_arr)):
+            for j in range(len(two_d_arr[i])):
+                if i == x and j == y:
+                    two_d_arr[i, j] = replace
+
     elif activity_choice == "Flood Fill":
-        x = st.sidebar.slider('y', 0, 2, 1)
-        st.write('Value of X:', x)
-        
-        y = st.sidebar.slider('x', 0, 2, 1)
-        st.write('Value of Y:', y)
-        
         replace = st.sidebar.slider('replace', 0, 1000, 500)
-        st.write('replace:', replace)
         
-        boundary_color = two_d_arr[x, y]  # Get the current color at (x, y)
-        
-        flood_fill(x, y, replace, boundary_color)
+        for x in range(3):
+            for y in range(3):
+                boundary_color = two_d_arr[x, y]  # Get the current color at (x, y)
+                flood_fill(x, y, replace, boundary_color)
+
+    fig = plt.figure()
+    img = plt.imshow(two_d_arr, cmap='rainbow', interpolation='none')
+    img.set_clim([1, 1000])
+    plt.colorbar()
+    plt.show()
+    st.pyplot(fig)
 
 if __name__ == '__main__':
     main()
