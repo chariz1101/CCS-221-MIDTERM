6709bc08d13d58f03e35c5bcbeacf5f61caff114
chiichann <116653047+chiichann@users.noreply.github.com>
2023-06-05 12:50:03 +0300

    Update activity2.py

--- pages/activity2.py
+++ pages/activity2.py
@@ -6,12 +6,12 @@ two_d_arr = np.array([[1, 0, 1],
                       [1, 0, 1],
                       [0, 1, 0]])
 
-def flood_fill(x, y, replace, boundary_color):
+def flood_fill(x, y, num_boxes, replace_color, boundary_color):
     rows, cols = two_d_arr.shape
     visited = set()
     stack = [(x, y)]
 
-    while stack:
+    while stack and num_boxes > 0:
         current_x, current_y = stack.pop()
 
         if (current_x, current_y) in visited:
@@ -20,7 +20,8 @@ def flood_fill(x, y, replace, boundary_color):
         visited.add((current_x, current_y))
 
         if two_d_arr[current_x, current_y] == boundary_color:
-            two_d_arr[current_x, current_y] = replace
+            two_d_arr[current_x, current_y] = replace_color
+            num_boxes -= 1
 
             neighbors = [
                 (current_x - 1, current_y),
@@ -38,7 +39,7 @@ def flood_fill(x, y, replace, boundary_color):
                     stack.append((neighbor_x, neighbor_y))
 
 def main():
-    st.title("This is Activity 2")
+    st.title("This is Activity 2 and Flood Fill")
 
     activity_choice = st.sidebar.selectbox(
         "Select an activity",
@@ -67,10 +68,12 @@ def main():
         y = st.sidebar.slider('x', 0, 2, 1)
         st.write('Value of Y:', y)
 
-        replace = st.sidebar.slider('replace', 0, 1000, 500)
-        st.write('replace:', replace)
+        num_boxes = st.sidebar.slider('Number of Boxes', 1, 9, 1)
+        replace = st.sidebar.slider('Color', 0, 1000, 500)
+        st.write('Number of Boxes:', num_boxes)
+        st.write('Color:', replace)
 
-        flood_fill(x, y, replace, two_d_arr[x, y])
+        flood_fill(x, y, num_boxes, replace, two_d_arr[x, y])
 
     fig = plt.figure()
     img = plt.imshow(two_d_arr, cmap='rainbow', interpolation='none')
